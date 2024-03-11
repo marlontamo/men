495ce93d40c2ce7a609f9bbeed9ac36c8975b93b
marlontamo <marlontamo2@gmail.com>
2024-03-11 19:54:25 +0800

    refactor server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const express = require("express");
-//const colors = require('colors');
 const dotenv = require("dotenv").config();
-//const { errorHandler } = require('./middleware/errorMiddleware');
-//const connectDB = require('./config/db');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -12,10 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
-// app.use("/api/users", require("./routes/userRoutes"));
-app.get("/api/goal", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+
 app.get("/", (req, res) => {
   res.send("wow server is now running!!!");
 });
